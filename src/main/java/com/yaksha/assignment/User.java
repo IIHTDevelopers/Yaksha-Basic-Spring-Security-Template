38f386a84bb99a446311a32be2814e2f2d6a12f0
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-05-19 15:48:22 +0530

    Update User.java

--- src/main/java/com/yaksha/assignment/User.java
+++ src/main/java/com/yaksha/assignment/User.java
@@ -1,14 +1,6 @@
 package com.yaksha.assignment;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-@Entity
 public class User {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String username;
